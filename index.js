d45094679d86c5c4a5932bebc73c479eb8a8cba6
Ricardo Paez <rickypaez84@gmail.com>
2023-03-28 20:42:52 -0300

    Express funcionando y ruta raiz GET definida

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola node")
+const express = require('express');
+const app = express();
+const port = 8453;
+
+app.get('/', (request, response) =>{
+    response.send({
+        msg:"Bienvenidos a mi servidos express",
+        ok:true
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor funcionando en puerto ${port}`);
+} )
